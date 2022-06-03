9edb57fbf8456f3711186f7b8b4e9ad4069b890e
perepichai <o.perepichai@gmail.com>
2022-06-03 10:09:51 +0300

    fix id

--- src/app/shared/models/book.model.ts
+++ src/app/shared/models/book.model.ts
@@ -1,14 +1,14 @@
 
 export class BookEntity {
-    id?: number | null;
+    id?: number;
     title: string;
     description: string;
     pageCount: number;
     excerpt?: string;
     publishDate: string;
 
-    constructor(description: string, title: string, pageCount: number, publishDate: string, id?: number | null, excerpt?: string) {
-        this.id = id;
+    constructor(description: string, title: string, pageCount: number, publishDate: string, id?: number, excerpt?: string) {
+        this.id = id || 0;
         this.description = description;
         this.title = title;
         this.excerpt = excerpt;
